30ebb5b1971046f9f3777b6c6a899eca82334268
Morris Yang <morris821028@gmail.com>
2014-05-28 08:08:00 +0800

    delete all

--- src/exception/NoSuchIDExceptions.java
+++
@@ -1,9 +0,0 @@
-package exception;
-/**
- * Throws when identity not found.
- * @author Yang
- *
- */
-public class NoSuchIDExceptions extends Exception {
-	private static final long serialVersionUID = 1L;
-}
